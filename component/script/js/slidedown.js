81366b1356510758ea3d617b686bb4631e3e8e92
Malone-Global-University <maloneglobaluniversity@gmail.com>
2025-08-26 13:53:22 -0700

    testing

--- component/script/js/slidedown.js
+++ component/script/js/slidedown.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("/component/html/navbar.html")
+  fetch("/component/partial/navbar/site-navabar.html")
     .then(res => res.text())
     .then(data => {
       document.getElementById("navbar-placeholder").innerHTML = data;
